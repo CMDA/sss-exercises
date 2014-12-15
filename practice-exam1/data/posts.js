2e9accdc183907428b17bbefa1cd7ce41ca0e021
Edwin van der Graaf <edwin@digitpaint.nl>
2014-12-15 22:23:49 +0100

    Proper naming for store

--- practice-exam1/data/posts.js
+++ practice-exam1/data/posts.js
@@ -4,32 +4,35 @@ var env = 'development';
 
 var dataSample = {
   'env': env,
-  'posts': [{
-    'id': 1,
-    'author': 'Blog author',
-    'title': 'Blog title'
-  }]
+  'store': {
+    'posts':[{
+      'id': 1,
+      'author': 'Blog author',
+      'title': 'Blog title'
+    }]
+  }
 };
 
-/* 101 - FS cached object */
+/* 101 - FS cached data store */
 module.exports = function(req, res, next){
 
-  var posts = [],
-      path = './tmp/data-set.json';
+  var store = {},
+      dir = './tmp/', // will likely crash, when using more then 1 lvl
+      fileName = 'data-set.json',
+      path = dir + fileName;
 
   // Since we have no data, load the sample
   if (!fs.existsSync(path)) {
-    fs.mkdirSync("./tmp");
-    posts = dataSample;
+    store = dataSample.store;
   } else {
     // Read file, can't continue without it,
     // so not much use making it async
-    posts = JSON.parse(fs.readFileSync(path, 'utf8')).posts;
+    store = JSON.parse(fs.readFileSync(path, 'utf8')).posts;
   }
 
   // Setup data
   req.locals = req.locals || {};
-  req.locals.posts = posts;
+  req.locals.store = store;
 
   // Keep a reference to original res.end
   var end = res.end;
@@ -39,14 +42,18 @@ module.exports = function(req, res, next){
   res.end = function(data, encoding){
     var dataSet = {
       env: env,
-      posts: posts
+      store: store
     };
 
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+    }
+
     // write json to tmp
-    fs.writeFile(path, JSON.stringify(dataSet), function(err) {
+    fs.writeFile(path, JSON.stringify(dataSet), {flag: 'w+'}, function(err) {
       if(err) {
         console.log(err);
-        console.log("Error saving data to disk, this may result in data lose.");
+        console.log('Error saving data to disk, this may result in data lose.');
       }
     });
 
