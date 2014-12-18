ef12c34f0489afd3a4d2f7dd7e0d043c323891ac
Edwin van der Graaf <edwin@digitpaint.nl>
2014-12-18 11:52:26 +0100

    Finish exercise 5

--- exercise5/README.md
+++ exercise5/README.md
@@ -9,10 +9,19 @@ In this exercise we will work with further with login for a user, making our app
 * [Download this exercise](http://cl.ly/0u3W0D3k0d07)
 * Run ```$ npm install``` to install the dependencies. 
 * Read the exercises, review the slides of [last lecture](http://cmda.github.io/sss-course/lesson5).
-* Review the code we've set up for you, this is the end result of _exercise 3_.
 * _It is advisable to type all code by hand_, and not to copy-paste the examples. By typing them by hand, helps you reason about every single line and makes it comprehensible. 
 
 ### Reading list
 * [Node. js in Action, Cantelon, Mike, et al. - Manning Publications, 2014](http://www.manning.com/cantelon/)
 
+
 ## Task 1
+We have taken the end result of _exercise 3_. In exercise 3 we worked with user authentication, however we only supported one user right now. By means of a database we will be able to authenticate multiple users.
+
+1. Use the SQL file to create the [initial tables with data](http://cmda.github.io/sss-course/lesson5#/3/6), you can find this file in the ```data/``` directory. 
+2. Add the dependencies of mysql to this project. [This weeks lecture](http://cmda.github.io/sss-course/lesson5#/6) helps you out with the basics.
+3. Display the list of all users in the database on ``` [GET] /users ```, when a user is logged in.
+4. Now update the ```[POST] /users/login``` route so that the users in our database are able to log in.
+
+### Extra task
+1. Create a sign up form where a new user can create an account.
--- exercise5/routes/users.js
+++ exercise5/routes/users.js
@@ -4,12 +4,14 @@ var router = express.Router();
 // Create routes for the following URL's
 
 // [GET] /users
-router.get("/", function(req, res){
+router.get('/', function(req, res){
   if(req.session.username){
-    res.send("Welcome, " + req.session.username);
+    res.render('users/index', {
+      title: 'Welcome, ' + req.session.username
+    });
   } else {
     // Redirect the user here
-    res.redirect("/users/login");
+    res.redirect('/users/login');
   }
 });
 
@@ -17,21 +19,35 @@ router.get("/", function(req, res){
 router.get('/login', function(req, res){
   res.render('users/login', {
     postUrl: '/users/login',
-    error: true
+    error: false
   });
 });
-
 
 // [POST] /users/login
 router.post('/login', function(req, res){
   var username = req.body.username;
   var password = req.body.password;
+  // We now authorize the use against a hardcoded one,
+  // in this task we want to authorize against the database table
+  // with users (see also data/database.sql)
   if(username === 'admin' &&
      password === '1234'){
     req.session.username = 'admin';
     res.redirect('/users');
   } else {
-    res.send('Wachtwoord of gebruikersnaam is niet juist');
+    res.render('users/login', {
+      postUrl: '/users/login',
+      error: 'Gebruikersnaam en/of wachtwoord onjuist.'
+    });
   }
 });
+
+// [GET] /users/logout
+router.get('/logout', function(req, res){
+  // Destroy the session
+  req.session.destroy();
+  // Redirect to index page
+  res.redirect('/users');
+});
+
 module.exports = router;
